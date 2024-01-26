2e01ef6df6258283caf6dd7aefc4d5de94fa860a
vladislavv27 <vladislavv27@gmail.com>
2024-01-26 23:56:48 +0200

    created template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { MainComponent } from './main/main.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule, 
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition, state } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  ngOnInit() {
+    this.applyRandomGradientToText();
+  }
+
+
+
+  applyRandomGradientToText() {
+    const randomColor1 = this.getRandomColor();
+    const randomColor2 = this.getRandomColor();
+  
+    const gradient = `linear-gradient(45deg, ${randomColor1}, ${randomColor2})`;
+  
+    const element = document.querySelector('.gradient-text') as HTMLElement;
+    if (element) {
+      element.style.backgroundImage = gradient;
+      element.style.webkitBackgroundClip = 'text';
+      element.style.backgroundClip = 'text';
+      element.style.color = 'transparent'; // Make the text transparent
+  
+      // Set a random gradient for the text color
+      element.style.backgroundSize = '100% 100%'; // Ensure the gradient covers the entire text
+      element.style.animation = 'textAnimation 2s forwards'; // Add animation for text reveal
+    }
+  }
+  
+  getRandomColor() {
+    let randomColor;
+    do {
+      randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    } while (this.isColorBlack(randomColor));
+    return randomColor;
+  }
+  
+  isColorBlack(color: string) {
+    return color.toLowerCase() === '#000000';
+  }
+  
+  
 
 }
